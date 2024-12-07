5cac0b933f5614d23515ca0d6b0cb02ed11758fb
faithk7u <faithk7u@outlook.com>
2024-12-07 17:03:10 -0500

    update action to make JSON DATA not required

--- .github/scripts/media.js
+++ .github/scripts/media.js
@@ -6,8 +6,11 @@ const jsonString = process.argv[3]
 const fileExists = fs.existsSync(filePath)
 
 const photos = fileExists ? JSON.parse(fs.readFileSync(filePath).toString()) : []
-const json = JSON.parse(jsonString)
-photos.push(json)
+// only add jsonString if it exists, otherwise it is a file upload
+if (jsonString) {
+  const json = JSON.parse(jsonString)
+  photos.push(json)
+}
 
 const dirName = path.dirname(filePath)
 if (!fileExists && dirName !== '.') {
